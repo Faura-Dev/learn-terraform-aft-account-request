557f2471a376dc0dc940bee5600ba0598bbc84c0
Henry Wanjala <wanjala.henry.dev@gmail.com>
2025-06-07 14:56:32 -0700

    Development Account Faura Property Self Assessment Account

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,39 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+# Development Faura Property Self Assessment Account
+module "dev_property_assessment" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "services+dev-property-self-assessment@faura.us"
+    AccountName               = "dev-property-self-assessment"
+    ManagedOrganizationalUnit = "Workloads/Development"
+    SSOUserEmail              = "services+dev-property-self-assessment@faura.us"
+    SSOUserFirstName          = "Development"
+    SSOUserLastName           = "Property Assessment"
+  }
+
+  account_tags = {
+    "account"     = "dev-property-selft-assessment"
+    "managed"     = "AFT"
+    "environment" = "development"
+    "application" = "faura-property-self-assessment"
+    "security"    = "high"
+    "compliance"  = "enterprise"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Henry Wanjala"
+    change_reason       = "Create Development Faura Property Self Assessment Account using AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group        = "development"
+    security_team = "enterprise-security"
+    app_team     = "property-assessment-team"
+  }
+
+  account_customizations_name = "dev-property-self-assessment"
+}
